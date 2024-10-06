2fa74b180c7660fdb94234e9cefb5b9087cfde05
Mai Linh <mailinhhn1310@gmail.com>
2024-10-06 11:11:10 +0700

    6. Main nav

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -84,7 +84,7 @@ function MainNav() {
         <li>
           <StyledNavLink to="/settings">
             <HiOutlineCog6Tooth />
-            Cài đặt
+            Cài đặt hệ thống
           </StyledNavLink>
         </li>
       </NavList>
