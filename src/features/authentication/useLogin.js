8c7b29a984338f9e5c25c594ea5cad0c939800fe
Mai Linh <mailinhhn1310@gmail.com>
2024-10-28 00:44:40 +0700

    15. Authorization: Protecing Routes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,10 +1,11 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) =>
@@ -13,6 +14,7 @@ export function useLogin() {
         password,
       }),
     onSuccess: (user) => {
+      queryClient.setQueriesData(["user"], user);
       console.log(user);
       navigate("/dashboard");
     },
